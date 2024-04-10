01fb2197c388ec83da16be65e2f636c101f5e2b8
alrikmunoz <50486916+alrikmunoz@users.noreply.github.com>
2024-04-10 19:25:15 +0200

    adapt styling for desktop/larger screens

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
     <main>
       <div className="p-6"> {/* This is div for the whole page content */}
 
-        <div className="w-4/5 mx-auto max-w-sm mt-8 mb-16"> {/* start of menu */}
+        <div className="w-4/5 mx-auto max-w-sm md:max-w-3xl mt-8 mb-16"> {/* start of menu */}
           <h2 id="menu" className={"text-center text-xl font-bold tracking-widest"}>Menú</h2>
 
-          <div className="my-4"> {/* start of menu content  (NOTE: DUMMY CONTENT!) */}
+          <div className="my-4 md:my-20"> {/* start of menu content  (NOTE: DUMMY CONTENT!) */}
             <h3 className="text-lg font-semibold mb-2">Appetizers</h3>
             <div className="flex justify-between mb-1">
               <span>Toast Skagen</span>
@@ -66,9 +66,9 @@ export default function Home() {
           </div>
         </div> {/* end of menu */}
         
-        <div className="w-4/5 mx-auto max-w-sm my-16"> {/* start of hours */}
+        <div className="w-4/5 mx-auto max-w-sm md:max-w-3xl my-16"> {/* start of hours */}
           <h2 id="hours" className={"text-center text-xl font-bold tracking-widest"}>Horario</h2>
-          <div className="my-4">
+          <div className="my-4 md:my-12">
             <div className="flex justify-between mb-1">
               <span>Mar–Jue</span>
               <span>11:00–22:00</span>
@@ -91,41 +91,42 @@ export default function Home() {
         </div>
 
 
-
-        <div className="w-4/5 mx-auto max-w-sm my-16"> {/* start of contact */}
-          <h2 id="contact" className={"text-center text-xl font-bold tracking-widest"}>Contacto</h2>
-          <div className="my-4">
-            <div className="flex justify-center gap-14 my-8">
-              <Link title="Página de Facebook para Casa Marta Restaurante" aria-label="Facebook" href="https://www.facebook.com/people/Casa-Marta-Restaurante/61557999488320/"><IconBrandFacebook /></Link>
-              <Link title="Página de Instagram para Casa Marta Restaurante" aria-label="Instagram" href="https://www.instagram.com/casamartarestaurante/"><IconBrandInstagram /></Link>
-            </div>
-            <Link href="mailto:casamartarestaurante@gmail.com" className="flex mb-1 ml-10">
-              <p className="basis-1/6"><IconMail /></p>
-              <p className="basis-5/6">casamartarestaurante@gmail.com</p>
-            </Link>
-            <Link href="tel:+340123456" className="flex mb-1 ml-10">
-              <p className="basis-1/6"><IconPhone /></p>
-              <p className="basis-5/6">+34 012 34 56</p>
-            </Link>
-            <div className="flex mb-1 ml-10">
-              <p className="basis-1/6"><IconMapPin /></p>
-              <p className="basis-5/6">Carrer Ramon Llull, 26, 08330 Premià de Mar</p>
-            </div>
-            <div className="my-6">
-              <p>Para realizar reservas, por favor utilice el número de teléfono indicado arriba.</p>
+        <div className="md:flex md:justify-center gap-48">
+          <div className="w-4/5 mx-auto md:mx-0 max-w-sm my-16"> {/* start of contact */}
+            <h2 id="contact" className={"text-center text-xl font-bold tracking-widest"}>Contacto</h2>
+            <div className="my-4">
+              <div className="flex justify-center gap-14 my-8 md:my-8">
+                <Link title="Página de Facebook para Casa Marta Restaurante" aria-label="Facebook" href="https://www.facebook.com/people/Casa-Marta-Restaurante/61557999488320/"><IconBrandFacebook /></Link>
+                <Link title="Página de Instagram para Casa Marta Restaurante" aria-label="Instagram" href="https://www.instagram.com/casamartarestaurante/"><IconBrandInstagram /></Link>
+              </div>
+              <Link href="mailto:casamartarestaurante@gmail.com" className="flex mb-1 ml-10">
+                <p className="basis-1/6"><IconMail /></p>
+                <p className="basis-5/6">casamartarestaurante@gmail.com</p>
+              </Link>
+              <Link href="tel:+340123456" className="flex mb-1 ml-10">
+                <p className="basis-1/6"><IconPhone /></p>
+                <p className="basis-5/6">+34 012 34 56</p>
+              </Link>
+              <div className="flex mb-1 ml-10">
+                <p className="basis-1/6"><IconMapPin /></p>
+                <p className="basis-5/6">Carrer Ramon Llull, 26, 08330 Premià de Mar</p>
+              </div>
+              <div className="my-6">
+                <p>Para realizar reservas, por favor utilice el número de teléfono indicado arriba.</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="w-4/5 mx-auto max-w-sm my-16"> {/* start of about */}
-          <h2 id="about" className={"text-center text-xl font-bold tracking-widest"}>Sobre nosotros</h2>
-          <div className="my-4">
-            <p className="text-center mb-6">En Casa Marta Restaurante, nuestra historia comienza con Marta, una colombiana que se mudó de Suecia a Barcelona hace {yearsAgoStr} años. 
-            Tras dejar atrás su carrera en tecnología, el sueño de Marta de compartir su pasión por la cocina diversa cobró vida.</p>
-            
-            <p className="text-center">Nuestro restaurante combina un poco de todo, desde tapas reconfortantes hasta platos peruanos tentadores. 
-            Arraigado en la calidez colombiana de Marta, Casa Marta irradia un ambiente amigable, familiar y acogedor para todos. 
-            Bienvenidos a una experiencia culinaria tan sincera como deliciosa.</p>  
+          <div className="w-4/5 mx-auto md:mx-0 max-w-sm my-16 md:my-24"> {/* start of about */}
+            <h2 id="about" className={"text-center text-xl font-bold tracking-widest"}>Sobre nosotros</h2>
+            <div className="my-4 md:my-12">
+              <p className="text-center mb-6">En Casa Marta Restaurante, nuestra historia comienza con Marta, una colombiana que se mudó de Suecia a Barcelona hace {yearsAgoStr} años. 
+              Tras dejar atrás su carrera en tecnología, el sueño de Marta de compartir su pasión por la cocina diversa cobró vida.</p>
+              
+              <p className="text-center">Nuestro restaurante combina un poco de todo, desde tapas reconfortantes hasta platos peruanos tentadores. 
+              Arraigado en la calidez colombiana de Marta, Casa Marta irradia un ambiente amigable, familiar y acogedor para todos. 
+              Bienvenidos a una experiencia culinaria tan sincera como deliciosa.</p>  
+            </div>
           </div>
         </div>
       </div>
--- app/ui/menu-nav.tsx
+++ app/ui/menu-nav.tsx
@@ -8,9 +8,9 @@ type MenuItemsType = {
 
 export default function MenuNav({ menuItems } : { menuItems : MenuItemsType}) {
   return (
-    <div className="flex-col justify-between items-center bg-white">
+    <div className="flex flex-col md:flex-row justify-between md:justify-evenly items-center bg-white">
       <Link
-      className="flex justify-center gap-2 items-center bg-white border-b border-b-slate-300 p-1 h-20 md:h-40 w-auto sticky top-0 z-10"
+      className="flex justify-center gap-2 items-center bg-white border-b md:border-b-0 border-b-slate-300 p-1 h-20 md:h-24 w-full md:w-auto sticky top-0 z-10"
       href="/">
         
         <Image
@@ -26,20 +26,20 @@ export default function MenuNav({ menuItems } : { menuItems : MenuItemsType}) {
           src="/logo_text.png"
           alt="Casa Marta Restaurante"
           className="h-full w-auto"
-          width={889}
-          height={232}
+          width={296.33} // 889
+          height={77.33} // 232
           priority
           />
         </h1>
       </Link>
       
       {/* Menu items: */}
-      <div className="flex gap-0 justify-stretch">
+      <div className="flex gap-0 justify-around w-full md:w-1/2 md:max-w-2xl">
         {menuItems.map((menuItem, i) => (
              <Link 
              key={i} 
              href={`#${menuItem.elementId}`}
-             className="group text-center flex-1 flex flex-col justify-center  transition ease-in-out delay-100 hover:-translate-y-0 hover:scale-100 duration-500"
+             className="group text-center flex-1 flex flex-col justify-center md:max-w-32 transition ease-in-out delay-100 hover:-translate-y-0 hover:scale-100 duration-500"
              >
               <span className="py-2 px-3 md:py-4 rounded-3xl md:group-active:transition-none group-active:decoration-[#708a6f] group-hover:underline decoration-[#708a6f] group-hover:decoration-[#bad7b9] group-hover:decoration-2 underline-offset-4 transition-all ease-in-out delay-100 duration-200">{menuItem.name}</span>
             </Link>
