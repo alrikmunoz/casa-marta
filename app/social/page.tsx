9c2d78ea448eec15b9865f3cb01c48888518d0df
alrikmunoz <50486916+alrikmunoz@users.noreply.github.com>
2024-04-20 17:13:32 +0200

    adjust social buttons sizing

--- app/social/page.tsx
+++ app/social/page.tsx
@@ -10,8 +10,8 @@ export default function Page() {
   return (
     <main>
       <div className="py-6 px-2 md:p-6"> {/* This is div for the whole page content */}
-        <h2 className="text-center text-4xl font-bold mt-10 mb-16 tracking-widest">Links útiles</h2>
-        <div className="flex flex-col justify-evenly items-center gap-8">
+        <h2 className="text-center text-4xl font-bold mt-6 mb-12 tracking-widest">Links útiles</h2>
+        <div className="flex flex-col justify-evenly items-center gap-7">
           <SocialButton newTab href="https://g.page/r/CZs-qTEFUZLpEBM/review">Reseñar en Google</SocialButton>
           <SocialButton newTab href="https://www.facebook.com/people/Casa-Marta-Restaurante/61557999488320/">Visitar Facebook</SocialButton>
           <SocialButton newTab href="https://www.instagram.com/casamartarestaurante">Seguir en Instagram</SocialButton>
--- app/ui/SocialButton.tsx
+++ app/ui/SocialButton.tsx
@@ -8,6 +8,12 @@ type SocialButtonProps = LinkProps & {
 export default function SocialButton({ children, newTab = false, href, ...props }: SocialButtonProps) {
   // TODO: combine/merge passed down className prop, instead of ignoring it
   return (
-    <Link href={href} target={newTab ? "_blank" : "_self"} className="transition duration-150 ease-out active:drop-shadow-lg active:bg-[#9db79c] hover:bg-[#bad7b9] hover:-translate-x-0.5 hover:-translate-y-0.5 text-[#4f7e4e] rounded-lg py-8 px-10 text-2xl border-4 border-[#4f7e4e] uppercase tracking-widest" {...props}>{children} &nbsp;-&gt;</Link>
+    <Link href={href} target={newTab ? "_blank" : "_self"} className="
+    transition duration-150 ease-out 
+    active:drop-shadow-lg active:bg-[#9db79c] hover:bg-[#bad7b9] hover:-translate-x-0.5 hover:-translate-y-0.5 
+    text-[#4f7e4e] rounded-lg 
+    py-6 px-6 text-xl 
+    border-4 border-[#4f7e4e] 
+    uppercase tracking-widest" {...props}>{children} &nbsp;-&gt;</Link>
   );
 }
