f27934080345f5cd1fb4008fe97a22efbf4f5eee
alrikmunoz <50486916+alrikmunoz@users.noreply.github.com>
2024-05-26 21:41:06 +0200

    make header more sticky, and use custom scoll func

--- app/ui/navmenu.tsx
+++ app/ui/navmenu.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -7,10 +9,33 @@ type MenuItemsType = {
 }[];
 
 export default function NavMenu({ menuItems } : { menuItems?: MenuItemsType}) {
+  const scrollTo = (e: React.MouseEvent, id: string) => {
+    e.preventDefault();
+    const el = document.getElementById(id);
+    let visibleHeader = "header-links";
+    
+    // on larger screens, current CSS makes the whole header stick rather than only the links (which have a smaller height)
+    const isLargeScreen = window.matchMedia('(min-width: 768px)')
+    if (isLargeScreen.matches) {
+      visibleHeader = "header";
+    }
+    const headerElement = document.getElementById(visibleHeader);
+
+    if (el && headerElement) {
+      const y = el.getBoundingClientRect().top + window.scrollY - headerElement.getBoundingClientRect().height - 18;
+      window.scroll({
+        top: y,
+        behavior: 'smooth'
+      });
+    }
+  }
+
   return (
-    <div className="flex flex-col md:flex-row justify-between md:justify-evenly items-center bg-white">
+    <div id="header" className="flex flex-col justify-between md:flex-row md:justify-evenly items-center bg-white sticky top-[-71px] md:top-0 shadow-[0_3px_20px_-6px_#dbdbdb]">
       <Link
-      className="flex justify-center gap-2 items-center bg-white border-b md:border-b-0 border-b-slate-300 p-1 h-20 md:h-24 w-full md:w-auto sticky top-0 z-10"
+      className="flex justify-center gap-2 items-center bg-white p-1 h-20 md:h-24 w-full md:w-auto z-10"
+      scroll={false}
+      onClick={() => window.scroll({top: 0, behavior: 'smooth'})}
       href="/">
         
         <Image
@@ -35,14 +60,15 @@ export default function NavMenu({ menuItems } : { menuItems?: MenuItemsType}) {
       
       {/* Menu items: */}
       {menuItems && (
-      <div className="flex gap-0 justify-around w-full md:w-1/2 md:max-w-2xl">
+      <div id="header-links" className="flex gap-0 justify-around w-full md:w-1/2 md:max-w-2xl">
         {menuItems.map((menuItem, i) => (
              <Link 
              key={i} 
+             onClick={e => scrollTo(e, menuItem.elementId)}
              href={`#${menuItem.elementId}`}
              className="group text-center flex-1 flex flex-col justify-center md:max-w-32 transition ease-in-out delay-100 hover:-translate-y-0 hover:scale-100 duration-500"
              >
-              <span className="py-2 px-3 md:py-4 rounded-3xl md:group-active:transition-none group-active:decoration-[#708a6f] group-hover:underline decoration-[#708a6f] group-hover:decoration-[#bad7b9] group-hover:decoration-2 underline-offset-4 transition-all ease-in-out delay-100 duration-200">{menuItem.name}</span>
+              <span className="pt-0 pb-3 px-3 md:py-4 rounded-3xl md:group-active:transition-none group-active:decoration-[#708a6f] group-hover:underline decoration-[#708a6f] group-hover:decoration-[#bad7b9] group-hover:decoration-2 underline-offset-4 transition-all ease-in-out delay-100 duration-200">{menuItem.name}</span>
             </Link>
         ))}
       </div>
